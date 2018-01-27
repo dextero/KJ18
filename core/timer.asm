0a166a761d384fc892dd0727da2d1204dc2b59ff
Marcin Radomski <marcin@mradomski.pl>
2018-01-27 23:51:09 +0100

    Simplify timer_get_elapsed

--- core/timer.asm
+++ core/timer.asm
@@ -8,6 +8,8 @@ timer_reset:
     sty TIMER_START_JIFFIES_HI
 
 
+; return jiffies (1/60 of a second) elapsed since timer_reset call
+; result saved to TIMER_START_JIFFIES (3B, unsigned)
 timer_get_elapsed:
     lda JIFFIES_LO
     ldx JIFFIES_MI
@@ -15,63 +17,12 @@ timer_get_elapsed:
 
     sec
     sbc TIMER_START_JIFFIES_LO
-    sta TIMER_ELAPSED_CENTISECONDS ; temporarily jiffies
+    sta TIMER_ELAPSED_JIFFIES_LO
     txa
     sbc TIMER_START_JIFFIES_MI
-    sta TIMER_ELAPSED_SECONDS
+    sta TIMER_ELAPSED_JIFFIES_MI
     tya
     sbc TIMER_START_JIFFIES_HI
-    sta TIMER_ELAPSED_MINUTES
-
-    ; if carry gets set, we have measured 24h+ interval, so screw it
-    ; at this point TIMER_ELAPSED_* contains elapsed time in jiffies
-
-    ; convert to seconds
-    lda TIMER_ELAPSED_MINUTES 
-    sta div24_dividend+2
-    lda TIMER_ELAPSED_SECONDS 
-    sta div24_dividend+1
-    lda TIMER_ELAPSED_CENTISECONDS
-    sta div24_dividend+0
-
-    lda #0
-    sta div24_divisor+2
-    sta div24_divisor+1
-    lda #60
-    sta div24_divisor+0
-
-    jsr div24
-
-    lda div24_remainder
-    sta TIMER_ELAPSED_CENTISECONDS ; jiffies!
-
-    jsr div24
-    lda div24_remainder
-    sta TIMER_ELAPSED_SECONDS
-
-    lda div24_dividend
-    sta TIMER_ELAPSED_MINUTES
-    ; ignore times over 255min, nobodu's gonna play that long
-
-    ; convert jiffies to centiseconds (*= 100/60)
-    lda TIMER_ELAPSED_CENTISECONDS
-    sta mul8_a
-    lda #100
-    sta mul8_b
-    jsr mul8
-
-    stx div24_dividend+0
-    sta div24_dividend+1
-    lda #0
-    sta div24_dividend+2
-
-    sta div24_divisor+2
-    sta div24_divisor+1
-    lda #60
-    sta div24_divisor+0
-
-    jsr div24
-    lda div24_dividend+0
-    sta TIMER_ELAPSED_CENTISECONDS
+    sta TIMER_ELAPSED_JIFFIES_HI
 
     rts
