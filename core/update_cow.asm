8a7a8a854359ad005044cad6845af3bcd57c0d44
Marcin Radomski <marcin@mradomski.pl>
2018-01-28 11:14:20 +0100

    Cow collisions, wip

--- core/update_cow.asm
+++ core/update_cow.asm
@@ -1,5 +1,14 @@
-update_cow:
+reset_cow:
+    lda #0
+    sta SPRITE_3_X
 
+    lda #1
+    sta COW_VISIBLE
+
+    rts
+
+
+update_cow:
     ; check if cow is visible
     lda COW_VISIBLE
     cmp #00
@@ -7,11 +16,35 @@ update_cow:
 
     inc SPRITE_3_X
 
-
     ; add bit to 255+ movement
     ; check if cow moved safely
+    lda #SCREEN_LINE_SIZE_PIX/2
+    cmp SPRITE_3_X
+    bcs .continue
 
+    lda #SCREEN_LINE_SIZE_PIX/2+COW_SPRITE_SIZE
+    cmp SPRITE_3_X
+    bcc .no_collision
 
+    lda CURRENT_SPEED
+    cmp #LETHAL_SPEED
+    bcc .no_collision
+
+    ; collision
+    lda #0
+    sta CURRENT_SPEED
+    jsr reset_cow
+    jmp .return
+
+.no_collision:
+    ; HACK
+    lda #<SCREEN_LINE_SIZE_PIX-1
+    cmp SPRITE_3_X
+    bne .continue
+
+    jsr reset_cow
+
+.continue
     lda COW_UNDERFLOW
     clc
     adc CURRENT_SPEED
@@ -22,7 +55,6 @@ update_cow:
     inc SPRITE_3_Y
 
     ; check if cow collided with train
-
 
 .return
     rts
